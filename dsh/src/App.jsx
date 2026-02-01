3a2ba42767be3a447793f0f2775e205330ad297a
mexhiss <mmexhis@gmail.com>
2026-02-01 15:05:47 +0100

    hello

--- dsh/src/App.jsx
+++ dsh/src/App.jsx
@@ -4,22 +4,34 @@ import './App.css'
 
 function App() {
 
-  const[hasNotification,setHasNotification]=useState(false)
-  function toggle(){
-    setHasNotification(!hasNotification)
-  }
 
-const[status,setstatus] = useState(false)
-function toggle2(){
-  setstatus(!status)
-}
-  return (
-   
-    <div>
-      <button onClick={toggle2}>Status : {status ? <span className='Active'>Aktiv</span> : <span className='Pasive'>Pasiv</span>}</button>
-      <button onClick={toggle}>{hasNotification ? 'Hide Notification' : ' Show Notification'}</button>
-      <p>{hasNotification && status ? <span className='tekst'>You have a new notification</span> : ''}</p>
-    </div>
+  const [name,setName]=useState('')
+  const [password,setPassword]=useState('')
+  const[email,setEmail]=useState("")
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if(!name || !password || !email){
+      alert("Fill all the fields")
+      return
+    }if(!email.includes('@')){
+      alert('you forgot @')
+      return
+    }alert(name+' '+ password+' '+email)
+  }
+  return(
+
+<div>
+  <form onSubmit={handleSubmit}>
+    <input value={name} onChange={e=>setName(e.target.value)} />
+    <input value={password}  onChange={e=>setPassword(e.target.value)
+    }/>
+    <input value={email} onChange={e=>setEmail(e.target.value)} />
+    <button type='submit'>submit</button>
+
+  </form>
+</div>
+
   )
 }
 
